9622bc229be1d588db7be1a0c1d557e252a0748d
Minh Vu <minhhv1@uci.edu>
2019-10-14 20:00:32 -0700

    implemented signup and login functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 // 1
 // importing required dependencies
@@ -20,10 +22,22 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// This middleware will be invoked ever time AplloClient sends request to server
+// First, we get the authentication token from localStorage if it exists; after that, we return the headers to the context so httpLink can read them.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // 3
 // instantiating ApolloClient by passing in httpLink and new cache instance
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
