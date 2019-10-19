21f66d8255264711d1d99957061961b1dd5d9aee
Minh Vu <minhhv1@uci.edu>
2019-10-18 17:29:00 -0700

    implemented voting functionality, doesn't update votes in UI though

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
 // stores the query
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -12,12 +12,31 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+  const data = store.readQuery({ query: FEED_QUERY })
+
+  const votedLink = data.feed.links.find(link => link.id === linkId)
+  votedLink.votes = createVote.link.votes
+
+  store.writeQuery({ query: FEED_QUERY, data })
+}
+	
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -27,11 +46,18 @@ class LinkList extends Component {
     
           const linksToRender = data.feed.links
     
-          return (
-            <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
-            </div>
-          )
+		return (
+			<div>
+				{linksToRender.map((link, index) => (
+				<Link
+				key={link.id}
+				link={link}
+				index={index}
+				updateStoreAfterVote={this._updateCacheAfterVote}
+				/>
+				))}
+			</div>
+		)
         }}
       </Query>
     )
